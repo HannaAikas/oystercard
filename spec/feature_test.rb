c1da76e7da21b85c983d0da01faa22e6e2a8be2b
dtrts <djt210@gmail.com>
2019-07-31 15:32:11 +0100

    Step11 Tests written

--- spec/feature_test.rb
+++ spec/feature_test.rb
@@ -1,36 +1,50 @@
 require './lib/oystercard'
 
-oystercard = Oystercard.new
-oystercard.top_up(15)
-puts oystercard.balance # this should puts 15
-oystercard.deduct(5)
-puts oystercard.balance # this should puts 10
-oystercard.touch_in # this should change in_journey? to true
-puts oystercard.in_journey # this should puts true
-oystercard.touch_out # this should change it back to false
-puts oystercard.in_journey # this should puts false
-puts Oystercard::MAX_VALUE # this should puts the max value we set
+# oystercard = Oystercard.new
+# oystercard.top_up(15)
+# puts oystercard.balance # this should puts 15
+# oystercard.deduct(5)
+# puts oystercard.balance # this should puts 10
+# oystercard.touch_in # this should change in_journey? to true
+# puts oystercard.in_journey # this should puts true
+# oystercard.touch_out # this should change it back to false
+# puts oystercard.in_journey # this should puts false
+# puts Oystercard::MAX_VALUE # this should puts the max value we set
 
-# step09
+# # step09
 
-card2 = Oystercard.new
-begin
-  card2.touch_in # expect to raise error
-rescue StandardError => e
-  puts 'This is good, has raised correct error for touching in without enough money'
-else
-  puts 'This is bad, feature not written to check touch in without enough money'
-end
+# card2 = Oystercard.new
+# begin
+#   card2.touch_in # expect to raise error
+# rescue StandardError => e
+#   puts 'This is good, has raised correct error for touching in without enough money'
+# else
+#   puts 'This is bad, feature not written to check touch in without enough money'
+# end
 
-# step 10
+# # step 10
 
-card3 = Oystercard.new
-card3.top_up(1)
-card3.touch_in
-begin
-  card3.touch_out # expect to raise error
-rescue
-  puts 'This is good, has raised an error when tried to touch out'
-else
-  puts 'This is bad, feature not written for touch_out checks'
-end
+# card3 = Oystercard.new
+# card3.top_up(1)
+# card3.touch_in
+# begin
+#   card3.touch_out # expect to raise error
+# rescue StandardError
+#   puts 'This is good, has raised an error when tried to touch out'
+# else
+#   puts 'This is bad, feature not written for touch_out checks'
+# end
+
+# step 11
+puts '--  step 11  ------------------------------------------------------------------'
+station = 'Lambeth'
+
+card4 = Oystercard.new
+
+card4.top_up(50)
+puts 'Entry station not nil on creation' unless card4.entry_station.nil?
+card4.touch_in(station)
+puts 'Entry station not set on touch in' if card4.entry_station != station
+card4.touch_out
+puts 'Entry station not cleared' unless card4.entry_station.nil?
+puts '-- Step 11 complete  ----------------------------------------------------------'
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,12 +1,18 @@
 require 'oystercard'
 
 describe Oystercard do
+  let(:station) { double('Station Name') }
+
   it 'new card has a balance of 0' do
     expect(subject.balance).to eq 0
   end
 
   it 'starts off with in_journey being false' do
     expect(subject.in_journey).to eq false
+  end
+
+  it 'has no entry station on creation' do
+    expect(subject.entry_station).to eq(nil)
   end
 
   describe '#top_up' do
@@ -21,14 +27,14 @@ describe Oystercard do
     end
   end
 
-  describe '#touch_in' do
+  describe '#touch_in(entry_station)' do
     it 'raises error on insufficient funds' do
-      expect { subject.touch_in }.to raise_error Oystercard::ERR_LOW_BALANCE
+      expect { subject.touch_in(station) }.to raise_error Oystercard::ERR_LOW_BALANCE
     end
   end
 
   context 'user is touched in' do
-    before(:each) { subject.top_up(50); subject.touch_in }
+    before(:each) { subject.top_up(50); subject.touch_in(station) }
 
     it 'journey has started' do
       expect(subject).to be_in_journey
@@ -41,6 +47,15 @@ describe Oystercard do
     it 'then card touched out, journey ends' do
       subject.touch_out
       expect(subject).not_to be_in_journey
+    end
+
+    it 'has recorded entry_station' do
+      expect(subject.entry_station).not_to be(nil)
+    end
+
+    it 'clears entry_station on touch_out' do
+      subject.touch_out
+      expect(subject.entry_station).to eq(nil)
     end
   end
 end
