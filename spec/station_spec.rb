95d21324bd6907e8c27769ede826d45bd05e31da
Kat <vampirebatkat@gmail.com>
2019-08-01 15:57:40 +0100

    Final Task 13

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -2,15 +2,14 @@ require 'station'
 
 describe Station do
 
-  #subject {described_class.new(name: "Old Street", zone: 1)}
-  station = Station.new("Old Street",1)
+  subject {described_class.new("Old Street", 1)}
 
   it 'knows its name' do
-    expect(station.name).to eq "Old Street"
+    expect(subject.name).to eq "Old Street"
   end
 
   it 'knows its zone' do
-    expect(station.zone).to eq 1
+    expect(subject.zone).to eq 1
   end
 
 end
