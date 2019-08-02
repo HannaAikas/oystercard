a20bd2f91ec17e3c5b70256c32bc7fe5fe5c49eb
Hanna <hannaaikas@gmail.com>
2019-08-02 14:52:41 +0100

    Step 14: Add feature test and first unit tests for calculating fare and completing journey

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -7,10 +7,6 @@ describe Journey do
     card = Oystercard.new
     before(:each) { card.top_up(10); card.touch_in("Aldgate"); card.touch_out("Ealing") }
 
-    it 'finishes a journey' do
-      expect(journeys[:entry_station]).to eq("Aldgate").and(journeys[:exit_station]).to eq("Ealing")
-    end
-
     it 'calculates a fare' do
         expect(subject.fare).to eq 2
     end
@@ -18,4 +14,5 @@ describe Journey do
     it 'knows if a journey is complete or not'
       expect(subject.complete?).to eq true
     end
+
 end
